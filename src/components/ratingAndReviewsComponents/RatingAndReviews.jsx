3545bfff098ff3676f3fe23f9f31ae76ad6dcf52
Soohyun Lim <d2axasap@gmail.com>
2020-11-15 19:56:54 -0500

    Make a function named getAllReviews which makes GET request to Reviews API to retrieve all reviews under given product ID. Added a functionality to sort by 'helpful', 'newest', and 'relevant'.

--- src/components/ratingAndReviewsComponents/RatingAndReviews.jsx
+++ src/components/ratingAndReviewsComponents/RatingAndReviews.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import NewReview from './NewReview.jsx';
 import ReviewList from './ReviewList.jsx';
 import Rating from './Rating.jsx';
@@ -6,12 +7,37 @@ import Rating from './Rating.jsx';
 class RatingAndReviews extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      productId: '',
+      reviews: []
+    };
+    this.getAllReviews = this.getAllReviews.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllReviews('5', 'relevant');
+  }
+
+  getAllReviews(productId, sortBy) {
+    return axios
+      .get('http://3.21.164.220/reviews', {
+        params: {
+          product_id: productId,
+          sort: sortBy
+        }
+      })
+      .then(({data}) => {
+        this.setState({
+          reviews: data.results
+        })
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <div>
+        {console.log(this.state)}
         <hr />
         <Rating />
         <hr />
