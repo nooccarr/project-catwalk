8297b04424535027564262a637459fc4bdc78b7a
Matt Martin <mjm2312@columbia.edu>
2020-11-16 15:47:24 -0500

    Thumbnail carousel should be working

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -124,30 +124,105 @@ let StyleSelector = ({styles}) => {
 class Gallery extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      thumbnails: '',
+      startIndex: 0,
+      endIndex: 4
+    }
+    this.slideThumbnailsDown = this.slideThumbnailsDown.bind(this);
+    this.slideThumbnailsUp = this.slideThumbnailsUp.bind(this);
+
+  }
+
+  slideThumbnailsDown() {
+  
+    if (this.state.endIndex >= this.state.thumbnails.length-1) {
+      return;
+    }
+
+    var newEnd = this.state.endIndex+=1
+    var newStart = this.state.startIndex+=1
+    this.setState({
+      endIndex: newEnd,
+      startIndex: newStart
+    })
+
+  }
+
+  slideThumbnailsUp() {
+  
+    if (this.state.startIndex <= 0) {
+      return;
+    }
+
+    var newEnd = this.state.endIndex-=1
+    var newStart = this.state.startIndex-=1
+    this.setState({
+      endIndex: newEnd,
+      startIndex: newStart
+    })
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+ 
+    if (this.state.thumbnails === '' && this.props.currentStyle!=='') {
+
+      console.log('in condition')
+       this.setState({
+       thumbnails: this.props.currentStyle.photos,
+     })
+    }
+
+
+
+    
+
+    // if (prevState.thumbnails === this.state.thumbnails && this.state.thumbnails !== '') {
+    //   console.log('in condition')
+    //    this.setState({
+    //    thumbnails: this.props.currentStyle.photos,
+    //    updated: true
+    //  })
+    // }
+
   }
 
 
   render() {
     //Is there a way to do this without conditional render? doesn't like img src
-    if (this.props.currentStyle.photos === undefined) {
+    if (this.state.thumbnails === '') {
     return (
       <div> </div>
     )
     } else {
+      console.log(this.props.currentStyle.photos, 'in else')
+
       return (
         <div>
-           <img src = {this.props.currentStyle.photos[0].url} id = 'galleryImage'/>
+          <img src = {this.state.thumbnails[0].url} id = 'galleryImage'/>
            <div id = 'galleryThumbnailColumn'> 
-           <ul className = 'overviewThumbnail'> 
-            {this.props.currentStyle.photos.map((x) => (
-            <li>
-              <span id = 'galleryThumbnailContainer'> 
-               <img src = {x.thumbnail_url} id = 'galleryThumbnail'/> 
-              </span>
-            </li> 
-            ))}
+           <button className = 'galleryThumbnailUp' onClick = {this.slideThumbnailsUp}>up</button>
+           <ul className = 'overviewThumbnailUL'> 
+            {this.props.currentStyle.photos.map((x, index) => {
+            console.log(index);
+
+            if (index > this.state.endIndex || index < this.state.startIndex) {
+              return <span></span>
+            }
+
+            return (
+              <li className = 'overviewThumbnailLI'>
+                <span id = 'galleryThumbnailContainer'> 
+                  <img src = {x.thumbnail_url} id = 'galleryThumbnail'/> 
+                </span>
+              </li> 
+            )}
+            )}
 
           </ul>
+          <button className = 'galleryThumbnailDown' onClick = {this.slideThumbnailsDown}>down</button>
         </div>
            
         </div>
