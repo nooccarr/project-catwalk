b88060bf6dd6f1d2eb3eab194f8fa21445c0f533
Soohyun Lim <d2axasap@gmail.com>
2020-11-18 15:38:02 -0500

    Another code clean up

--- utils/average.js
+++ utils/average.js
@@ -1,14 +1,14 @@
 const average = (obj) => {
   let sum = 0;
-  let total = 0;
+  let totalCount = 0;
   for (let score = 1; score <= 5; score++) {
     let count = obj.ratings[score];
     if (count) {
       sum += count * score;
-      total += count;
+      totalCount += count;
     }
   }
-  return sum / total;
+  return sum / totalCount;
 };
 
 export default average;
