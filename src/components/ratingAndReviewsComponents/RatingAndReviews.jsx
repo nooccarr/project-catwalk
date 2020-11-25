984353ea477d1ab0fc1faba435fed4fad133c748
Soohyun Lim <d2axasap@gmail.com>
2020-11-24 23:59:38 -0500

    Created separate file for ReviewListEntry component. Added report functionality to individual review tile. Clicking on report hides clicked review in the database, then re-renders reviews list

--- src/components/ratingAndReviewsComponents/RatingAndReviews.jsx
+++ src/components/ratingAndReviewsComponents/RatingAndReviews.jsx
@@ -99,7 +99,7 @@ class RatingAndReviews extends React.Component {
   render() {
     return (
       <div>
-        {console.log(this.state)}
+        {/* {console.log(this.state)} */}
         <hr />
         <div>RATINGS & REVIEWS</div>
         <div>
